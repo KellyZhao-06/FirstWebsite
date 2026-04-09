23c8ca77d9626b7c7e469dca7d8fdad988392747
KellyZhao-06 <zhaokelly06@gmail.com>
2026-04-08 20:50:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Website loaded");
+function openWindow(id) {
+  document.getElementById(id).classList.remove("hidden");
+}
+
+function closeWindow(id) {
+  document.getElementById(id).classList.add("hidden");
+}
